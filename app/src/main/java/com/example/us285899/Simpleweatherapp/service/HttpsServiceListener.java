4c83fac164c3740c5e4936c80a8166070200ffc8
US285899 <u.usharani@gmail.com>
2017-03-10 17:26:00 +0000

    cleanup

--- app/src/main/java/com/example/us285899/Simpleweatherapp/service/HttpsServiceListener.java
+++ app/src/main/java/com/example/us285899/Simpleweatherapp/service/HttpsServiceListener.java
@@ -1,13 +1,9 @@
 package com.example.us285899.Simpleweatherapp.service;
 
-import com.example.us285899.Simpleweatherapp.model.WeatherInfo;
-
-import java.util.ArrayList;
-
 /**
  * Created by US285899 on 3/9/2017.
  */
 
 public interface HttpsServiceListener {
-    public void onResponse(String response);
+    void onResponse(String response);
 }
